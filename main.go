8abc1a8db84850a16301f29f8ddd7df6406ff24f
Andrew Obuchowicz <aobuchow@redhat.com>
2022-12-09 01:37:57 -0500

    Output updated DW to stdout, add option to update DW on cluster
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>

--- main.go
+++ main.go
@@ -13,21 +13,24 @@ import (
 	"k8s.io/client-go/util/homedir"
 
 	clusterClient "github.com/AObuchow/dw-update/customClient"
-	dw "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
+	dwv1alpha2 "github.com/devfile/api/v2/pkg/apis/workspaces/v1alpha2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/printers"
 	"sigs.k8s.io/yaml"
 )
 
 const (
-	NAMESPACE               string = "devworkspace-controller"
-	usage                   string = "Takes as input an existing DevWorkspace and the path to a Devfile and prints to stdout a DevWorkspace object, identical to the orginal one, but with the template replaced by the Devfile content (with a few gotchas).\n\nUsage:\n  dw-update [options]\n\nOptions:\n  -d, --devfile=[]:\n    The file that contains the new devfile that is going to be applied.\n  -w, --devworkspace=[]:\n    The name of the original DevWorkspace object that is going to be used to create the new DevWorkspace.\n"
-	devFileArgHelpMessage   string = "The file that contains the new devfile that is going to be applied."
-	devworkspaceHelpMessage string = "The name of the original DevWorkspace object that is going to be used to create the new DevWorkspace"
+	NAMESPACE                      string = "devworkspace-controller"
+	usage                          string = "Takes as input an existing DevWorkspace and the path to a Devfile and prints to stdout a DevWorkspace object (which can be optionally applied to the cluster), identical to the orginal one, but with the template replaced by the Devfile content (with a few gotchas).\n\nUsage:\n  dw-update [options]\n\nOptions:\n  -d, --devfile=[]:\n    The file that contains the new devfile that is going to be applied.\n  -w, --devworkspace=[]:\n    The name of the original DevWorkspace object that is going to be used to create the new DevWorkspace.\n"
+	devFileArgHelpMessage          string = "The file that contains the new devfile that is going to be applied."
+	devworkspaceHelpMessage        string = "The name of the original DevWorkspace object that is going to be used to create the new DevWorkspace"
+	updateClusterObjectHelpMessage string = "Whether the DevWorkspace object on the cluster should be updated with the new DevWorkspace"
 )
 
-func main() {
+var yamlPrinter printers.YAMLPrinter = printers.YAMLPrinter{}
 
-	devfilePath, devworkspaceName := parseArgs()
+func main() {
+	devfilePath, devworkspaceName, updateClusterObject := parseArgs()
 	devfile := loadDevfileOrPanic(*devfilePath)
 
 	config, err := getKubeConfig()
@@ -51,7 +54,7 @@ func main() {
 	dw, err := client.DevWorkspace(NAMESPACE).Get(*devworkspaceName, metav1.GetOptions{})
 	if err != nil {
 		if k8sErrors.IsNotFound(err) {
-			fmt.Fprintf(os.Stderr, "Couldn't find DevWorkspace with name: %s", *devworkspaceName)
+			fmt.Fprintf(os.Stderr, "Couldn't find DevWorkspace with name %s on the cluster", *devworkspaceName)
 			os.Exit(1)
 		}
 		panic(err)
@@ -60,11 +63,18 @@ func main() {
 	// Get the devworkspace from cluster
 	if dw != nil {
 		fmt.Println("Found the dw with given name: " + dw.Name)
-		updateDevWorkspace(dw, devfile, client)
+		updateDevWorkspace(dw, devfile, *updateClusterObject, client)
+		printDevWorkspace(dw)
 	}
 }
 
-// Get kube client depending on whether we're in a pod or running locally
+func printDevWorkspace(dw *dwv1alpha2.DevWorkspace) {
+	fmt.Println("Resulting DevWorkspace: ")
+	dw.GetObjectKind().SetGroupVersionKind(dwv1alpha2.SchemeGroupVersion.WithKind("DevWorkspace"))
+	yamlPrinter.PrintObj(dw, os.Stdout)
+}
+
+// Get kube config depending on whether we're in a pod or running locally
 func getKubeConfig() (*rest.Config, error) {
 	config, err := rest.InClusterConfig()
 	if err != nil {
@@ -96,19 +106,19 @@ func OutClusterConfig() (config *rest.Config, err error) {
 	return config, nil
 }
 
-func loadDevfileOrPanic(filePath string) dw.Devfile {
+func loadDevfileOrPanic(filePath string) dwv1alpha2.Devfile {
 	bytes, err := os.ReadFile(filePath)
 	if err != nil {
 		panic(err)
 	}
-	var devfile dw.Devfile
+	var devfile dwv1alpha2.Devfile
 	if err := yaml.Unmarshal(bytes, &devfile); err != nil {
 		panic(err)
 	}
 	return devfile
 }
 
-func updateDevWorkspace(dw *dw.DevWorkspace, devfile dw.Devfile, client *clusterClient.ExampleV1Alpha1Client) {
+func updateDevWorkspace(dw *dwv1alpha2.DevWorkspace, devfile dwv1alpha2.Devfile, updateClusterObject bool, client *clusterClient.ExampleV1Alpha1Client) {
 	// Preserve devworkspace spec.template.originalProjects
 	originalProjects := dw.Spec.Template.Projects
 
@@ -148,19 +158,25 @@ func updateDevWorkspace(dw *dw.DevWorkspace, devfile dw.Devfile, client *cluster
 	}
 
 	// Update devworkspace on cluster
-	_, err := client.DevWorkspace(NAMESPACE).Update(dw, metav1.UpdateOptions{})
+	if updateClusterObject {
+		_, err := client.DevWorkspace(NAMESPACE).Update(dw, metav1.UpdateOptions{})
 
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Printf("Succesfully updated devworkspace %s\n", dw.Name)
 	}
 }
 
-func parseArgs() (*string, *string) {
+func parseArgs() (*string, *string, *bool) {
 	devfilePath := flag.String("d", "", devFileArgHelpMessage)
 	flag.StringVar(devfilePath, "devfile", *devfilePath, devFileArgHelpMessage)
 
 	devworkspaceName := flag.String("w", "", devworkspaceHelpMessage)
 	flag.StringVar(devworkspaceName, "devworkspace", *devworkspaceName, devworkspaceHelpMessage)
+
+	updateClusterObject := flag.Bool("u", false, updateClusterObjectHelpMessage)
+	flag.BoolVar(updateClusterObject, "update", *updateClusterObject, updateClusterObjectHelpMessage)
 
 	flag.Usage = func() {
 		fmt.Fprint(flag.CommandLine.Output(), usage)
@@ -177,5 +193,5 @@ func parseArgs() (*string, *string) {
 		fmt.Println("The name of the devworkspace you want to update must be given.")
 		os.Exit(1)
 	}
-	return devfilePath, devworkspaceName
+	return devfilePath, devworkspaceName, updateClusterObject
 }
